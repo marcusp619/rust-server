63d2e5b1edfb168d58c2d0f6068ba8793b445dbd
marcusp619 <marcusp619@gmail.com>
2023-01-23 20:50:00 -0500

    create http module

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,57 @@
+use server::Server;
+use http::request::Request;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
+mod server {
+    pub struct Server {
+        addr: String,
+    }
+    
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self {
+                addr
+            }
+        }
+    
+        pub fn run(self) {
+            println!("Listening on on {}", self.addr)
         }
     }
+}
 
-    fn run(self) {
-        println!("Listening on on {}", self.addr)
+mod http {
+    pub mod request {
+        use super::method::Method;
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: Method,
+        }
+    }
+    
+   pub mod method {
+        // Represents the types of methods we allow on this server
+        pub enum Method {
+            GET,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
     }
 }
+// Represents the incoming request
+
+/*
+GET /user?id-10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
