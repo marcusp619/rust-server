efec71f9fb52bec580fc970e76a71c36c9333089
marcusp619 <marcusp619@gmail.com>
2023-01-25 19:18:45 -0500

    Add modules for request and method

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,12 @@
+use http::Request;
+use http::Method;
 use server::Server;
-use http::request::Request;
 
+mod server;
+mod http;
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-mod server {
-    pub struct Server {
-        addr: String,
-    }
-    
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self {
-                addr
-            }
-        }
-    
-        pub fn run(self) {
-            println!("Listening on on {}", self.addr)
-        }
-    }
-}
-
-mod http {
-    pub mod request {
-        use super::method::Method;
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: Method,
-        }
-    }
-    
-   pub mod method {
-        // Represents the types of methods we allow on this server
-        pub enum Method {
-            GET,
-            POST,
-            PUT,
-            HEAD,
-            CONNECT,
-            OPTIONS,
-            TRACE,
-            PATCH,
-        }
-    }
 }
 // Represents the incoming request
 
