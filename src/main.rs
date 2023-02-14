84493b371c015058b4818bc1db2b7399639ac755
marcusp619 <marcusp619@gmail.com>
2023-02-13 21:05:39 -0500

    update modules

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use http::Request;
-use http::Method;
 use server::Server;
 
-mod server;
 mod http;
+mod server;
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
